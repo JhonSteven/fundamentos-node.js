7434b4d32430c0c99372bb102d3dfc62f31d7d6f
Jhon Parra <jhonstevenparrapena@gmail.com>
2020-01-10 22:25:42 -0500

    Usando la consola node para ver cómo se ejecuta

---
+++ clase-1/clase-1.js
@@ -0,0 +1,13 @@
+console.log("Hello world!!");
+
+let i = 0;
+
+setInterval(function() {
+    console.log(i);
+    i++;
+
+    if (i === 5) {
+        console.log("forzamos error");
+        var a = 3 + z;
+    }
+}, 1000);
